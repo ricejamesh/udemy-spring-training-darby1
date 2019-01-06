220c17cad122f74c6985e611b0c363f5ddf319b3
James Rice <James.Rice@ttg-solutions.com>
2019-01-06 17:35:19 -0500

    Using the spring Model.

--- spring-mvc/src/main/java/com/ttgsolutions/springdemo/HelloWorldController.java
+++ spring-mvc/src/main/java/com/ttgsolutions/springdemo/HelloWorldController.java
@@ -1,7 +1,10 @@
 package com.ttgsolutions.springdemo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class HelloWorldController {
@@ -13,6 +16,24 @@ public class HelloWorldController {
 
     @RequestMapping("/processForm")
     public String processForm() {
+        return "helloworld";
+    }
+
+    @RequestMapping("/processFormVersionTwo")
+    public String letsShoutDude(HttpServletRequest request, Model model) {
+
+        //read the request parameter from the HTML form
+        String theName = request.getParameter("studentName");
+
+        // convert the data to all caps
+        theName = theName.toUpperCase();
+
+        // create the message
+        String result = "Yo! " + theName;
+
+        // add the message to the model.
+        model.addAttribute("message", result);
+
         return "helloworld";
     }
 }
--- spring-mvc/web/WEB-INF/view/helloworld.jsp
+++ spring-mvc/web/WEB-INF/view/helloworld.jsp
@@ -16,5 +16,9 @@
     <br><br>
 
     Student name: ${param.studentName}
+
+    <br><br>
+
+    The message: ${message}
 </body>
 </html>
