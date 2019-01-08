3461608011c830786c93b010a365d01088a93f16
James Rice <James.Rice@ttg-solutions.com>
2019-01-07 20:17:52 -0500

    Added a few simple log messages.

--- spring-mvc/src/main/java/com/ttgsolutions/springdemo/HomeController.java
+++ spring-mvc/src/main/java/com/ttgsolutions/springdemo/HomeController.java
@@ -3,11 +3,18 @@ package com.ttgsolutions.springdemo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.logging.Logger;
+
 @Controller
 public class HomeController {
 
+    private Logger logger = Logger.getLogger("HomeController");
+
     @RequestMapping("/")
     public String showPage() {
+
+        logger.info("--- logger.INFO message from HomeController. ---");
+        System.out.println("--- System.out.println from HomeController.  ----");
         return "main-menu";
     }
 }
