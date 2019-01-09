6f0db0938d5fcc125e3f5b8b8d277e3982266db4
James Rice <James.Rice@ttg-solutions.com>
2019-01-09 16:34:13 -0500

    Add InitBinder to automatically remove whitespace from parameters.

--- spring-mvc/src/main/java/com/ttgsolutions/springdemo/CustomerController.java
+++ spring-mvc/src/main/java/com/ttgsolutions/springdemo/CustomerController.java
@@ -1,8 +1,11 @@
 package com.ttgsolutions.springdemo;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,6 +14,17 @@ import javax.validation.Valid;
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+
+    // add an initbinder ... to convert trim input strings
+    // remove leading and trailing whitespace
+    // resolve issue for our validation
+
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
 
     @RequestMapping("/showForm")
     public String showForm(Model theModel) {
@@ -24,7 +38,7 @@ public class CustomerController {
             @Valid @ModelAttribute("customer") Customer theCustomer,
             BindingResult theBindingResult) {
 
-        System.out.println("lastName: |" + theCustomer.getLastName() + "|");
+        System.out.println("lastName: |" + theCustomer.getLastName() + "| ");
 
         if (theBindingResult.hasErrors()) {
             return "customer-form";
