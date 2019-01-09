65f141153852a9accda60647e2d3a4df08ac09b4
James Rice <James.Rice@ttg-solutions.com>
2019-01-09 16:42:22 -0500

    Added validation for int with min/max values.

--- spring-mvc/src/main/java/com/ttgsolutions/springdemo/Customer.java
+++ spring-mvc/src/main/java/com/ttgsolutions/springdemo/Customer.java
@@ -1,15 +1,20 @@
 package com.ttgsolutions.springdemo;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 public class Customer {
     private String firstName;
 
-
     @NotNull(message="is required")
     @Size(min=1, message="is required")
     private String lastName;
+
+    @Min(value = 0, message = "must be greater than or equal to zero")
+    @Max(value = 100, message = "must be less than 100")
+    private int freePasses;
 
     public String getFirstName() {
         return firstName;
@@ -25,5 +30,13 @@ public class Customer {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public int getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(int freePasses) {
+        this.freePasses = freePasses;
     }
 }
