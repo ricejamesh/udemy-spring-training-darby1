9e566a56ae33c70434ad55dfc1db7e6338a29901
James Rice <James.Rice@ttg-solutions.com>
2019-01-09 16:58:59 -0500

    Added spring validation of null integer

--- spring-mvc/src/main/java/com/ttgsolutions/springdemo/Customer.java
+++ spring-mvc/src/main/java/com/ttgsolutions/springdemo/Customer.java
@@ -9,9 +9,10 @@ public class Customer {
     @Size(min=1, message="is required")
     private String lastName;
 
+    @NotNull(message="is required")
     @Min(value = 0, message = "must be greater than or equal to zero")
     @Max(value = 100, message = "must be less than 100")
-    private int freePasses;
+    private Integer freePasses;
 
     @Pattern(regexp="^[a-zA-Z0-9]{5}", message="only 5 chars/digits")
     private String postalCode;
@@ -32,11 +33,11 @@ public class Customer {
         this.lastName = lastName;
     }
 
-    public int getFreePasses() {
+    public Integer getFreePasses() {
         return freePasses;
     }
 
-    public void setFreePasses(int freePasses) {
+    public void setFreePasses(Integer freePasses) {
         this.freePasses = freePasses;
     }
 
