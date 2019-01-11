83f52f2e20a9bcddd73b4825c4513a7cf9cbc424
James Rice <James.Rice@ttg-solutions.com>
2019-01-10 21:13:17 -0500

    Added basic hibernate example of writing a new entry to student table.

---
+++ hibernate-tutorial/src/main/java/com/ttgsolutions/springdemo/hibernate/TestHibernate.java
@@ -0,0 +1,44 @@
+package com.ttgsolutions.springdemo.hibernate;
+
+import com.ttgsolutions.springdemo.entity.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class TestHibernate {
+
+    public static void main(String[] args) {
+
+        // Create session factory
+        SessionFactory factory = new Configuration()
+                                    .configure("hibernate.cfg.xml")
+                                    .addAnnotatedClass(Student.class)
+                                    .buildSessionFactory();
+
+        // Create session
+        Session session = factory.getCurrentSession();
+
+        // use the session object to save Java object
+        try {
+
+            // create a student object
+            System.out.println("Creating a new student object...");
+            //Student tempStudent = new Student("Paul", "Wall", "Paul@email.com");
+            Student tempStudent = new Student("Ringo", "Star", "Ringo@email.com");
+
+            // start the transaction
+            session.beginTransaction();
+
+            // save the student object
+            System.out.println("Saving the student...");
+            session.save(tempStudent);
+
+            // commit transaction
+            session.getTransaction().commit();
+
+            System.out.println("Done!");
+        } finally {
+            factory.close();
+        }
+    }
+}
--- hibernate-tutorial/src/main/java/com/ttgsolutions/springdemo/jdbc/TestJdbc.java
+++ hibernate-tutorial/src/main/java/com/ttgsolutions/springdemo/jdbc/TestJdbc.java
@@ -4,16 +4,13 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class TestJdbc {
+
     public static void main(String[] args) {
-
-
-
         String jdbcUrl = "jdbc:postgresql://localhost/hb_student_tracker?ssl=false";
         String user = "hbstudent";
         String pass = "hbstudent";
 
         try {
-
             System.out.println("Connecting to database: " + jdbcUrl);
 
             Connection connection =
@@ -24,6 +21,5 @@ public class TestJdbc {
         } catch (Exception exc) {
             exc.printStackTrace();
         }
-
     }
 }
