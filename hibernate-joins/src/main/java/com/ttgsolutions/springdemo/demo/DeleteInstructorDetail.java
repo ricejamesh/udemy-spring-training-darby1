46186e873b2002757448b14f5923707b60be0059
James Rice <James.Rice@ttg-solutions.com>
2019-02-10 16:45:51 -0500

    Delete cascade by deleting InstructorDetail.

--- hibernate-joins/src/main/java/com/ttgsolutions/springdemo/demo/DeleteInstructorDetail.java
+++ hibernate-joins/src/main/java/com/ttgsolutions/springdemo/demo/DeleteInstructorDetail.java
@@ -6,7 +6,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class BiDirectionalDemo {
+public class DeleteInstructorDetail {
 
     public static void main(String[] args) {
 
@@ -32,9 +32,15 @@ public class BiDirectionalDemo {
             InstructorDetail tempInstructorDetail =
                     session.get(InstructorDetail.class, instructorDetailID);
 
-            System.out.println("tempInstructorDetail = " + tempInstructorDetail);
+            System.out.println("Retrieved InstructorDetail = " + tempInstructorDetail);
 
             System.out.println("Instructor: " + tempInstructorDetail.getInstructor());
+
+
+            System.out.println("Deleting tempInstructorDetail " + tempInstructorDetail);
+
+            // Delete instructor detail
+            session.delete(tempInstructorDetail);
 
             // commit transaction
             session.getTransaction().commit();
